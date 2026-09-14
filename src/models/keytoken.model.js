5d958975ede98ec1b051070048404412b3dfa2c8
hoang-helios <dvhoang2106@gmail.com>
2026-09-14 20:21:20 +0700

    updatge shop api

--- src/models/keytoken.model.js
+++ src/models/keytoken.model.js
@@ -1,13 +1,13 @@
 'use strict';
 
-const { Schema, model } = require('mongoose'); 
+const { Schema, Types } = require('mongoose'); 
 
 const DOCUMENT_NAME = 'KeyToken';
 const COLLECTION_NAME = 'keytokens';
 
 var keyTokenSchema = new Schema({
   user: {
-    type: Schema.Types.ObjectId,
+    type: Types.ObjectId,
     ref: 'Shop',
     required: true
   },
@@ -24,4 +24,4 @@ var keyTokenSchema = new Schema({
   collection: COLLECTION_NAME
 });
 
-module.exports = model(DOCUMENT_NAME, keyTokenSchema);
+module.exports = mongoose.model(DOCUMENT_NAME, keyTokenSchema);
